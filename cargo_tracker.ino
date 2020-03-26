d90a3775bf2de3c9e28cac3f2ed559f554537954
emfierro <60270967+emfierro@users.noreply.github.com>
2020-03-27 01:42:26 +0200

    prepared to use

--- cargo_tracker.ino
+++ cargo_tracker.ino
@@ -1,16 +1,24 @@
+#include <SoftwareSerial.h>
+
 #include <EEPROM.h>
 
 #define EEPROM_size 1024
 #define maxNumberCount 5
+#define GSM_speed 9600
+#define NEO6m_speed 9600 
+SoftwareSerial GPS_serial(7, 8); // RX, TX
 
 int PIR_sensor=5;
 char phoneNumberBuffer[11];
 byte cntPoint=(EEPROM_size-(maxNumberCount*12))-1;
-byte dataLength,EE_temp;
+byte dataLength,EE_temp,dtp;
 bool isHigh=false;
 uint32_t tempTime,timer;
-char txSMS[]="text to send via sms";
+uint32_t coordinates[2];//  N, E
 
+char txSMS[]="text to send via sms";
+char GPS_data[128];
+byte GPS_dataLength;
 #define clrSR delay(20);while(Serial.available()){delay(4);Serial.read();}
 void setup() {
 
@@ -30,7 +38,7 @@ void setup() {
   uploadLog_handler();
   dataLength=0;
   EEPROM.write(cntPoint,0);
-
+  GPS_serial.begin(NEO6m_speed);
 
 
   
@@ -52,6 +60,83 @@ bool findOK(){
       return false;
     
   }
+
+bool checkSum(){
+   byte xorSSum=GPS_data[0];
+   byte xorSCand;
+   if(GPS_data[GPS_dataLength-2]>'9'){xorSCand=GPS_data[GPS_dataLength-2]-'A'+10;}else{xorSCand=GPS_data[GPS_dataLength-2]-'0';}
+   xorSCand*=0x10;
+   if(GPS_data[GPS_dataLength-1]>'9'){xorSCand+=GPS_data[GPS_dataLength-1]-'A'+10;}else{xorSCand+=GPS_data[GPS_dataLength-1]-'0';}
+   
+           for(byte K=1;K<GPS_dataLength-3;K++){xorSSum^=GPS_data[K];}
+           if(xorSSum==xorSCand){return true;}return false;
+  
+  }
+void GPS_handler(){
+    if(GPS_serial.available()){
+        if(GPS_serial.read()=='$'){
+            GPS_dataLength=0;
+            timer=millis()+1500;
+            while(timer>millis()&&GPS_dataLength<128){
+                if(GPS_serial.available()){GPS_data[GPS_dataLength]=GPS_serial.read();GPS_dataLength++;}
+                if(GPS_dataLength>2&&GPS_data[GPS_dataLength-3]=='*'){break;}
+              }
+              if(checkSum()){
+                  if(!memcmp(GPS_data,"GPGLL",5)){
+                    
+                    getDataPointer(0,dtp);
+                    if(GPS_data[dtp]!=','){
+                        coordinates[0]=GPS_data[dtp+2]-'0';coordinates[0]*=10;
+                        coordinates[0]=GPS_data[dtp+3]-'0';coordinates[0]*=10;
+                        coordinates[0]=GPS_data[dtp+5]-'0';coordinates[0]*=10;
+                        coordinates[0]=GPS_data[dtp+6]-'0';coordinates[0]*=10;
+                        coordinates[0]=GPS_data[dtp+7]-'0';coordinates[0]*=10;
+                        coordinates[0]=GPS_data[dtp+8]-'0';coordinates[0]*=10;
+                        coordinates[0]=GPS_data[dtp+9]-'0';coordinates[0]*=1000;
+                        coordinates[0]/=60;
+                        coordinates[0]+=((uint32_t)(GPS_data[dtp+0]-'0'))*1e7;
+                        coordinates[0]+=((uint32_t)(GPS_data[dtp+1]-'0'))*1e6;
+                      } 
+                      getDataPointer(2,dtp);
+                    if(GPS_data[dtp]!=','){
+                        coordinates[1]=GPS_data[dtp+3]-'0';coordinates[1]*=10;
+                        coordinates[1]=GPS_data[dtp+4]-'0';coordinates[1]*=10;
+                        coordinates[1]=GPS_data[dtp+6]-'0';coordinates[1]*=10;
+                        coordinates[1]=GPS_data[dtp+7]-'0';coordinates[1]*=10;
+                        coordinates[1]=GPS_data[dtp+8]-'0';coordinates[1]*=10;
+                        coordinates[1]=GPS_data[dtp+9]-'0';coordinates[1]*=10;
+                        coordinates[1]=GPS_data[dtp+10]-'0';coordinates[1]*=1000;
+                        coordinates[1]/=60;
+                        coordinates[1]+=((uint32_t)(GPS_data[dtp+0]-'0'))*1e8;
+                        coordinates[1]+=((uint32_t)(GPS_data[dtp+1]-'0'))*1e7;
+                        coordinates[1]+=((uint32_t)(GPS_data[dtp+2]-'0'))*1e6;
+                      }
+                   // Serial.print(dtp);
+                    
+
+                     
+                       //coordinates
+                    }
+                }
+
+          
+          }
+        
+      }
+  
+  
+  }
+
+byte getDataPointer( byte indx, byte *outp){
+    for(byte T=5;T<(GPS_dataLength-3);T++){
+        if(GPS_data[T]==','){
+            if(indx){indx--;continue;}
+            outp=T+1;
+          }
+      }
+     
+  }
+  
 void connfigureM590(){
 
     while(true){Serial.println("AT"); if(findOK()){break;}} 
